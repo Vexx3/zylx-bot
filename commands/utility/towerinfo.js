1a1dc0b4126cb0e697453336d3f392f87e1f4d27
kelzy <94034445+Vexx3@users.noreply.github.com>
2024-10-24 11:57:12 +0800

    Update towerinfo.js

--- commands/utility/towerinfo.js
+++ commands/utility/towerinfo.js
@@ -1,15 +1,15 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { fetch } = require("undici");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("towerinfo")
     .setDescription("Get information about a specific JToH Tower.")
-    .addStringOption(option => 
+    .addStringOption((option) =>
       option
         .setName("tower")
         .setDescription("The name of the tower you want information about.")
-        .setRequired(true)
+        .setRequired(true),
     ),
   async execute(interaction) {
     const towerName = interaction.options.getString("tower");
@@ -23,32 +23,72 @@ module.exports = {
       const difficulty = extractInfo(data, "Difficulty");
       const length = extractInfo(data, "Length", true);
       const creator = extractInfo(data, "Creator(s)");
+      const imageUrl = extractImage(data);
 
-      console.log(difficulty, length, creator);
+      console.log(difficulty, length, creator, imageUrl);
 
-      if (!length | !difficulty | !creator) {
-        await interaction.reply({ content: "Could not retrieve tower information. Please check the tower name and try again.", ephemeral: true });
+      if (!length | !difficulty | !creator | !imageUrl) {
+        await interaction.reply({
+          content:
+            "Could not retrieve tower information. Please check the tower name and try again.",
+          ephemeral: true,
+        });
         return;
       }
 
-      const towerDetails = `**Tower Name:** ${towerName}\n**Length:** ${length}\n**Creator(s):** ${creator}`;
+      const towerEmbed = new EmbedBuilder()
+        .setColor("#0099ff")
+        .setTitle(`Information about ${towerName}`)
+        .setThumbnail(imageUrl)
+        .addFields(
+          { name: "Length", value: length },
+          { name: "Difficulty", value: difficulty },
+          { name: "Creator(s)", value: creator },
+        )
+        .setFooter({ text: "Source: Juke's Towers of Hell Wiki." });
 
-      await interaction.reply(towerDetails);
+      await interaction.reply({ embeds: [towerEmbed] });
     } catch (error) {
       console.error(error);
-      await interaction.reply({ content: "There was an error fetching the tower information. Please try again later.", ephemeral: true });
+      await interaction.reply({
+        content:
+          "There was an error fetching the tower information. Please try again later.",
+        ephemeral: true,
+      });
     }
   },
 };
 
 function extractInfo(html, label, isLength = false) {
   if (isLength) {
-    const regex = new RegExp(`<h3 class="pi-data-label pi-secondary-font">${label}</h3>\\s*<div class="pi-data-value pi-font">(.*?)</div>`, 's');
+    const regex = new RegExp(
+      `<h3 class="pi-data-label pi-secondary-font">${label}</h3>\\s*<div class="pi-data-value pi-font">(.*?)</div>`,
+      "s",
+    );
     const match = html.match(regex);
-    return match ? match[1].replace(/&lt;/g, '<').replace(/&gt;/g, '>').trim() : null;
+    return match
+      ? match[1].replace(/&lt;/g, "<").replace(/&gt;/g, ">").trim()
+      : null;
   } else {
-    const regex = new RegExp(`<h3 class="pi-data-label pi-secondary-font">${label}</h3>\\s*<div class="pi-data-value pi-font">(.*?)</div>`, 's');
+    const regex = new RegExp(
+      `<h3 class="pi-data-label pi-secondary-font">${label}</h3>\\s*<div class="pi-data-value pi-font">(.*?)</div>`,
+      "s",
+    );
     const match = html.match(regex);
     return match ? match[1].trim() : null;
   }
+}
+
+function extractCreators(html) {
+  const regex =
+    /<h3 class="pi-data-label pi-secondary-font">Creator\(s\)<\/h3>\s*<div class="pi-data-value pi-font"><span id="Owner"><b>(.*?)<\/b><\/span><\/div>/s;
+  const match = html.match(regex);
+  return match ? match[1].trim() : null;
+}
+
+function extractImage(html) {
+  const regex =
+    /<div class="wds-tab__content wds-is-current">\s*<figure class="pi-item pi-image">\s*<a href="(.*?)"/s;
+  const match = html.match(regex);
+  return match ? match[1].trim() : null;
 }
